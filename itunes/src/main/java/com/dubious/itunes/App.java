a88db91689383cc4aded018fae8ffbcbfed3aa13
Brian Dubois <brian.dubois@gmail.com>
2011-12-18 19:18:25 +0100

    Created itunes project with maven.

---
+++ itunes/src/main/java/com/dubious/itunes/App.java
@@ -0,0 +1,13 @@
+package com.dubious.itunes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
